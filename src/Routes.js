18f4ef298b906d680ccd0953d5f83e6b6f2ea536
Akhil Babu <realakhilbabu@gmail.com>
2021-06-27 21:33:08 +0530

    hashrouter instead of browserrouter

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import {Switch, Route, Redirect , HashRouter} from "react-router-dom";
 import IntroComponent from "./App.js";
 import WorkComponent from './Work.js'
 // import BlogComponent from './Blog'
@@ -7,15 +7,15 @@ import WorkComponent from './Work.js'
 
 const Routes = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
-        <Route exact path = '/'><Redirect to='/home' /> </Route>
+        <Route exact path = '/'> <Redirect to='/home' /> </Route>
         <Route exact path="/home" component={IntroComponent} />
         <Route exact path="/work" component={WorkComponent} />
         {/* <Route exact path="/blog" component={BlogComponent} />
         <Route exact path="/contact" component={ContactComponent} /> */}
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
