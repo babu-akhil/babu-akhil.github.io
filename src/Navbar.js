ea2b9b8422ae10eb734ff66f72561181919c9e55
Akhil Babu <realakhilbabu@gmail.com>
2021-06-27 22:07:35 +0530

    changes in routing

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,30 +2,17 @@ import React, {useState, useEffect} from 'react'
 
 function Navbar() {
 
-    let [active, setActive] = useState('home')
-    let pages = ['home', 'work', 'blog', 'contact']
+    let pages = ['work', 'blog', 'contact']
 
-    function toggleActive(e) {
-        console.log(e.target.innerText.toLowerCase())
-        setActive(e.target.innerText.toLowerCase())
-    }
 
     return(
         <div className = 'Navbar'>
             <ul>
             {pages.map(page => {
-                if(page===active){
-                    return(<li className = 'active'>
-                        <a href = {'/'+page==='home'?'':page} onClick = {toggleActive}>{page}</a>
-                    </li>
-                    )
-                }
-                else {
                     return(<li>
-                        <a href = {'/'+page==='home'?'':page} onClick = {toggleActive}>{page}</a>
+                        <a href = {'/'+page}>{page}</a>
                     </li>)
                 }
-            }
             )
             }
             </ul>
